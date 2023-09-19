e6654b2bf68b2672fd390780816e433dacbd02f9
geomontarroyos <gmontarroyosa@gmail.com>
2023-09-18 21:31:50 -0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-'use client';
+'use client'
 import { useState } from "react";
-import handlerAcessUser from "./functions/handlerAcess";
+import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import styles from "../globals.css";
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Login() {
   const [user, setUser] = useState({
